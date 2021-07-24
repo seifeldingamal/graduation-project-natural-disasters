e29ef1634798c44cd2d8b50442b2074c24a0c4b6
MohamedKh7 <69756365+MohamedKh7@users.noreply.github.com>
2021-07-24 20:22:31 +0200

    Update Home.js

--- webapp/client/src/views/Home.js
+++ webapp/client/src/views/Home.js
@@ -8,35 +8,38 @@ class Home extends Component {
         return (
             <div id='Body'>
                 <div id="Header">
-                    <div>
-                        <h1>Natural<br />Disaster</h1>
+                    <div id="content">
+                        <h1>Natural<br />Disasters</h1>
                     </div>
                 </div>
                 <div id="main3">
                     <div className='globe'>
                         <img src={logo} className="App-logo" alt="logo" />
                     </div>
-                        <Link 
-                            to='/stats'
-                            className='button'
-                        ><h5>View Disasters and Their Statistics</h5></Link>
-                        <Link 
-                            to='/predict'
-                            className='button'
-                        ><h5>Predict Future Shockwave Radius</h5></Link>                      
-                        <Link 
-                            to='/analysis'
-                            className='button'
-                        ><h5>Analysis Process</h5></Link>
-                        <Link 
-                            to='/usercheck'
-                            className='button'
-                        >UserCheck</Link>
-                </div>
-                <div id="Footer">
+                    <form action='/stats'className='button'>
+                        <button >
+                            <h5>View Disasters<br/>and<br/>Their Statistics</h5>
+                        </button>
+                    </form> 
+                    <form action='/predict'className='button'>   
+                        <button>
+                            <h5>Predict Future<br/>Shockwave Radius</h5>
+                        </button>
+                    </form>    
+                    <form action='/analysis'className='button'>                          
+                        <button>
+                            <h5>Analysis<br/>Process</h5>
+                        </button>
+                    </form> 
+                    <form action='/usercheck'className='button'>   
+                        <button >
+                            UserCheck
+                        </button>
+                    </form>    
+                
                     <b>Welcome to our system</b>
                     <p>
-                    Being able to predict Earthquakes could allow us <br />                                                  to better protect human life and property.
+                    Being able to predict Earthquakes could allow us <br /> to better protect human life and property.
                     </p>
                 </div>
             </div>
