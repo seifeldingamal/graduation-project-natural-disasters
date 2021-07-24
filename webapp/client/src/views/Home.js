188acaa47035d83782831f923e7c0964d04fa812
Seifeldin Abouelella <69943178+seifeldingamal@users.noreply.github.com>
2021-07-25 00:48:10 +0200

    Tags ready

--- webapp/client/src/views/Home.js
+++ webapp/client/src/views/Home.js
@@ -6,63 +6,65 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
     render() {
         return (
-            <div id='Body'>
-                <div id="Header">
-                    <div id="content">
-                        <h1>Natural<br />Disasters</h1>
-                    </div>
-                    <div id="main3">
-                        <div className='globe'>
-                            <img src={logo} className="App-logo" alt="logo" />
+            <div className="Home">
+                <div id='Body'>
+                    <div id="Header">
+                        <div id="content">
+                            <h1>Natural<br />Disasters</h1>
                         </div>
-                            <Link 
-                                to='/stats'
-                                className='button'
-                            ><h5>View Disasters and Their Statistics</h5></Link>
-                            <Link 
-                                to='/predict'
-                                className='button'
-                            ><h5>Predict Future Shockwave Radius</h5></Link>                      
-                            <Link 
-                                to='/analysis'
-                                className='button'
-                            ><h5>Analysis Process</h5></Link>
-                            <Link 
-                                to='/usercheck'
-                                className='button'
-                            >UserCheck</Link>
-                    </div>
-                    <div id="Footer">
+                        <div id="main3">
+                            <div className='globe'>
+                                <img src={logo} className="App-logo" alt="logo" />
+                            </div>
+                                <Link 
+                                    to='/stats'
+                                    className='button'
+                                ><h5>View Disasters and Their Statistics</h5></Link>
+                                <Link 
+                                    to='/predict'
+                                    className='button'
+                                ><h5>Predict Future Shockwave Radius</h5></Link>                      
+                                <Link 
+                                    to='/analysis'
+                                    className='button'
+                                ><h5>Analysis Process</h5></Link>
+                                <Link 
+                                    to='/usercheck'
+                                    className='button'
+                                >UserCheck</Link>
+                        </div>
+                        <div id="Footer">
+                            <b>Welcome to our system</b>
+                            <p>
+                            Being able to predict Earthquakes could allow us <br />                                                  to better protect human life and property.
+                            </p>
+                        </div>
+                        <form action='/stats'className='button'>
+                            <button >
+                                <h5>View Disasters<br/>and<br/>Their Statistics</h5>
+                            </button>
+                        </form> 
+                        <form action='/predict'className='button'>   
+                            <button>
+                                <h5>Predict Future<br/>Shockwave Radius</h5>
+                            </button>
+                        </form>    
+                        <form action='/analysis'className='button'>                          
+                            <button>
+                                <h5>Analysis<br/>Process</h5>
+                            </button>
+                        </form> 
+                        <form action='/usercheck'className='button'>   
+                            <button >
+                                UserCheck
+                            </button>
+                        </form>    
+                    
                         <b>Welcome to our system</b>
                         <p>
-                        Being able to predict Earthquakes could allow us <br />                                                  to better protect human life and property.
+                        Being able to predict Earthquakes could allow us <br /> to better protect human life and property.
                         </p>
                     </div>
-                    <form action='/stats'className='button'>
-                        <button >
-                            <h5>View Disasters<br/>and<br/>Their Statistics</h5>
-                        </button>
-                    </form> 
-                    <form action='/predict'className='button'>   
-                        <button>
-                            <h5>Predict Future<br/>Shockwave Radius</h5>
-                        </button>
-                    </form>    
-                    <form action='/analysis'className='button'>                          
-                        <button>
-                            <h5>Analysis<br/>Process</h5>
-                        </button>
-                    </form> 
-                    <form action='/usercheck'className='button'>   
-                        <button >
-                            UserCheck
-                        </button>
-                    </form>    
-                
-                    <b>Welcome to our system</b>
-                    <p>
-                    Being able to predict Earthquakes could allow us <br /> to better protect human life and property.
-                    </p>
                 </div>
             </div>
         )
--- webapp/client/src/views/Stats.js
+++ webapp/client/src/views/Stats.js
@@ -30,98 +30,72 @@ class Stats extends Component {
         let rightOpen = this.state.rightOpen ? 'open' : 'closed';
         
         return (
-            <div id="layout1">
-                <div id='left' className={leftOpen} >
-                    <div className='icon1'
-                        onClick={this.toggleSidebar} >
-                        &equiv;
-                    </div>
-                    <div className={`sidebar ${leftOpen}`} >
-                        <div className='header1'>
-                            <h3 className='title1'>
-                                Navigation
-                            </h3>
+            <div className="Stats">
+                <div id="layout1">
+                    <div id='left' className={leftOpen} >
+                        <div className='icon1'
+                            onClick={this.toggleSidebar} >
+                            &equiv;
                         </div>
-                        <div className='content1'>
-                                <Link 
-                                    to='/predict'
-                                    className='button1'
-                                ><h5>Predict Future<br/>Shockwave Radius</h5></Link>                
-                                <Link 
-                                    to='/analysis'
-                                    className='button1'
-                                ><h5>Analysis<br/>Process</h5></Link>
-                        </div>
-                    </div>
-                </div>
-                <div id='main1'>
-                    <div className="header1">
-                        <Link to='/'className='button1'>
-                            <h3 className={`
-                                title1
-                                ${'left-' + leftOpen}
-                                ${'right-' + rightOpen}
-                            `}>
-                                Natural<br/>Disasters
-                            </h3>
-                        </Link>
-                    </div>
-                    <div className="content1">
-                        {this.state.events.length > 0?
-                        <MapComp events={this.state.events} />
-                        : 'Data is loading'
-                        }
-                        <div>
-                            <figure>
-                                <img src="Graph_of_largest_earthquakes_1906-2005.png" alt="Circular Sector"/>
-                                <figcaption>Graph of largest earthquakes at the period<br/>(1906-2005)</figcaption>
-                            </figure>
-                            <figure>
-                                <img src="unnamed.gif" alt="Histogram"/>
-                                <figcaption>Number of Earthquakes by Year</figcaption>
-                            </figure>
+                        <div className={`sidebar ${leftOpen}`} >
+                            <div className='header1'>
+                                <h3 className='title1'>
+                                    Navigation
+                                </h3>
+                            </div>
+                            <div className='content1'>
+                                    <Link 
+                                        to='/predict'
+                                        className='button1'
+                                    ><h5>Predict Future<br/>Shockwave Radius</h5></Link>                
+                                    <Link 
+                                        to='/analysis'
+                                        className='button1'
+                                    ><h5>Analysis<br/>Process</h5></Link>
+                            </div>
                         </div>
                     </div>
-                </div>        
-                <div id='right' className={rightOpen} >
-                    <div className='icon1'
-                        onClick={this.toggleSidebar} >
-                        &equiv;
-                    </div>
-                    <div className={`sidebar ${rightOpen}`} >
-                        <div className='header1'>
-                            <h3 className='title1'>
-                            Filter Data
-                            </h3>
+                    <div id='main1'>
+                        <div className="header1">
+                            <Link to='/'className='button1'>
+                                <h3 className={`
+                                    title1
+                                    ${'left-' + leftOpen}
+                                    ${'right-' + rightOpen}
+                                `}>
+                                    Natural<br/>Disasters
+                                </h3>
+                            </Link>
                         </div>
-                        <div className='content1'>
-                            <h3>Determine Period</h3>
-                            <form className='form-input'>
-                                <input type="radio" id="RD1" name="Period" value="last 30 Days"/>
-                                <label htmlFor="RD1">last 30 Days</label><br/>
-                                <input type="radio" id="RD2" name="Period" value="last 3 Months"/>
-                                <label htmlFor="RD1">last 3 Months</label><br/>
-                                <input type="radio" id="RD3" name="Period" value="last 3 Years"/>
-                                <label htmlFor="RD1">last 3 Years</label><br/>
-                            </form>
-                            <br/>
-                            <form className='form-select'>
-                                <select id="Period">
-                                    <option>Select Specificly</option>
-                                    <option value="1980 - 1990">1980 - 1990</option>
-                                    <option value="1990 - 2000">1990 - 2000</option>
-                                    <option value="2000 - 2010">2000 - 2010</option>
-                                    <option value="2010 - 2020">2010 - 2020</option>
-                                </select>
-                            </form>
+                        <div className="content1">
+                            {this.state.events.length > 0?
+                            <MapComp events={this.state.events} />
+                            : 'Data is loading'
+                            }
+                            <div>
+                                <figure>
+                                    <img src="Graph_of_largest_earthquakes_1906-2005.png" alt="Circular Sector"/>
+                                    <figcaption>Graph of largest earthquakes at the period<br/>(1906-2005)</figcaption>
+                                </figure>
+                                <figure>
+                                    <img src="unnamed.gif" alt="Histogram"/>
+                                    <figcaption>Number of Earthquakes by Year</figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>        
+                    <div id='right' className={rightOpen} >
+                        <div className='icon1'
+                            onClick={this.toggleSidebar} >
+                            &equiv;
                         </div>
                         <div className={`sidebar ${rightOpen}`} >
-                            <div className='header'>
-                                <h3 className='title'>
+                            <div className='header1'>
+                                <h3 className='title1'>
                                 Filter Data
                                 </h3>
                             </div>
-                            <div className='content'>
+                            <div className='content1'>
                                 <h3>Determine Period</h3>
                                 <form className='form-input'>
                                     <input type="radio" id="RD1" name="Period" value="last 30 Days"/>
@@ -142,8 +116,36 @@ class Stats extends Component {
                                     </select>
                                 </form>
                             </div>
-                        </div>
-                    </div>                
+                            <div className={`sidebar ${rightOpen}`} >
+                                <div className='header'>
+                                    <h3 className='title'>
+                                    Filter Data
+                                    </h3>
+                                </div>
+                                <div className='content'>
+                                    <h3>Determine Period</h3>
+                                    <form className='form-input'>
+                                        <input type="radio" id="RD1" name="Period" value="last 30 Days"/>
+                                        <label htmlFor="RD1">last 30 Days</label><br/>
+                                        <input type="radio" id="RD2" name="Period" value="last 3 Months"/>
+                                        <label htmlFor="RD1">last 3 Months</label><br/>
+                                        <input type="radio" id="RD3" name="Period" value="last 3 Years"/>
+                                        <label htmlFor="RD1">last 3 Years</label><br/>
+                                    </form>
+                                    <br/>
+                                    <form className='form-select'>
+                                        <select id="Period">
+                                            <option>Select Specificly</option>
+                                            <option value="1980 - 1990">1980 - 1990</option>
+                                            <option value="1990 - 2000">1990 - 2000</option>
+                                            <option value="2000 - 2010">2000 - 2010</option>
+                                            <option value="2010 - 2020">2010 - 2020</option>
+                                        </select>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>                
+                    </div>
                 </div>
             </div>
         )
